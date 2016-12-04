b7754391e6047653e21c0ee636bbee0aa0848e82
Brett Toomey <brettcwx@gmail.com>
2016-12-04 13:24:04 -0800

    Boilerplate. Re-enabled macOS builds and added codecoverage.

--- Sources/Helpers.swift
+++ Sources/Helpers.swift
@@ -1,5 +1,5 @@
-/*import JSON
 import Foundation
+/*import JSON
 
 //TODO(Brett): change implementation to C since foundation is broken on Linux
 func parseJSONFile(path: String) throws -> JSON {
@@ -10,3 +10,46 @@ func parseJSONFile(path: String) throws -> JSON {
     )
     return json
 }*/
+
+public enum Day: Int {
+	case today
+	case sunday
+	case monday
+	case tuesday
+	case wednesday
+	case thursday
+	case friday
+	case saturday
+}
+
+typealias Time = (hour: Int, minute: Int)
+
+extension Int {
+    var am: Time {
+        return (hour: self, minute: 0)
+    }
+    
+    var pm: Time {
+        guard self <= 12 && self != 0 else {
+            return (hour: self, minute: 0)
+        }
+        
+        return (hour: self + 12, minute: 0)
+    }
+}
+
+extension Date {
+    static func today() -> (Day?, Int?, Int?) {
+        let components = Calendar(identifier: .gregorian).dateComponents([.weekday, .hour, .minute], from: Date())
+        
+        guard
+            let weekday = components.weekday,
+            let hour = components.hour,
+            let minute = components.minute
+        else {
+            return (nil, nil, nil)
+        }
+        
+        return (Day(rawValue: weekday), hour, minute)
+    }
+}
