d2e42f21883b27c1d9b21f05fbab68f0c7e8ad61
Brett Toomey <brettcwx@gmail.com>
2017-03-30 12:25:20 +0200

    Cleaned up Package.swift

--- Package.swift
+++ Package.swift
@@ -1,10 +1,5 @@
 import PackageDescription
 
-let vaporBeta = Version(2,0,0, prereleaseIdentifiers: ["beta"])
-
 let package = Package(
-    name: "Jobs",
-	dependencies: [
-		//.Package(url: "https://github.com/vdka/JSON", majorVersion: 0, minor: 16)
-	]
+    name: "Jobs"
 )
